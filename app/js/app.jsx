72662271ee161587ed50ccc3f5293009f66e18eb
Ivan Storck <ivanoats@gmail.com>
2014-12-04 07:00:02 -0800

    component with one avatar working

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,33 +1,39 @@
 /** @jsx React.DOM */
 
-// var $ = require('jquery');
+var $ = require('jquery');
 var _ = require('lodash');
 var React = require('react/addons');
 // allow React Dev tools to work
 window.React = React;
 
-var getGithubData = require('./getGithubData');
+var Githubbers = React.createClass({
+  getInitialState: function() {
+    return {
+      login: '',
+      avatarUrl: ''
+    }
+  },
 
-var getOwners = data => _.pluck(data, 'owner');
-var getAvatars = owners => _.pluck(owners, 'avatar_url');
-
-var main = function main (data) {
-  var owners = getOwners(data);
-  console.log('Owners: ', owners );
-  console.log('Avatar URLs: ', getAvatars( owners ))
-};
-
-getGithubData('https://api.github.com/repositories?since=1000', main);
-var imageURL = "https://avatars.githubusercontent.com/u/614?v=3";
-
-var Githubber = React.createClass({
   render: function() {
     return (
-        <div className="col-md-1 avatar">
-          <img src={imageURL} alt="avatar"/>
-        </div>
+      <div className="col-md-1 avatar">
+        <img src={this.state.avatarUrl} alt="avatar"/>
+      </div>
     )
+  },
+
+  componentDidMount: function() {
+    $.getJSON(this.props.source, function (result) {
+      var firstRepo = result[0];
+      var firstOwner = firstRepo.owner;
+      if (this.isMounted()) {
+        this.setState({
+          login: firstOwner.login,
+          avatarUrl: firstOwner.avatar_url
+        });
+      }
+    }.bind(this));
   }
 });
 
-React.render(<Githubber/>, document.getElementById('mount-point'));
+React.render(<Githubbers source="https://api.github.com/repositories?since=1000"/>, document.getElementById('mount-point'));
